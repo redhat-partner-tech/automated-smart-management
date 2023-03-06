410d78a9f3a52332b561a0b38b6343650679a779
Mike Savage <mike.savage@gmail.com>
2023-03-06 02:18:33 -0500

    adjust plaform-python symlink

--- build/ee_buildah_aap2_22dev.sh
+++ build/ee_buildah_aap2_22dev.sh
@@ -8,7 +8,7 @@ BASEIMAGEOWNER=ansible-automation-platform-22
 BASEIMAGENAME=ee-supported-rhel8
 BIVERSION=1.0.0-229
 IMAGE=ee-automated-smart-mgmt-aap2-dev
-VERSION=1.0.0
+VERSION=1.0.1
 START_DIR=$(pwd)
 TMP_WRKDIR=$(mktemp -d /tmp/XXXXXXXX)
 ctr=$(buildah from registry.redhat.io/$BASEIMAGEOWNER/$BASEIMAGENAME:$BIVERSION)
@@ -17,6 +17,8 @@ buildah run $ctr /bin/sh -c 'python3 -m pip install jinja2==3.1.1'
 buildah run $ctr /bin/sh -c 'python3 -m pip install apypie'
 buildah run $ctr /bin/sh -c 'python3 -m pip install psycopg2-binary'
 buildah run $ctr /bin/sh -c 'python3 -m pip install requests==2.28.1'
+buildah run $ctr /bin/sh -c 'rm /usr/libexec/platform-python3.6'
+buildah run $ctr /bin/sh -c 'ln -s /usr/bin/python3 /usr/libexec/platform-python3.6'
 cd $TMP_WRKDIR
 git clone https://github.com/redhat-partner-tech/automated-smart-management.git
 cd automated-smart-management
